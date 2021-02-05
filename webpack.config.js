4aad3f8a18c33375a1a4b8e6f57b72273d125782
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2021-02-05 11:52:18 +0800

    navbar base added

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/script.js",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist",
+        assetModuleFilename: "images/[name][ext][query]",
+    },
+    module: {
+        rules:[
+            {
+                test: /\.m?js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            }
+        ]
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false}),
+        new MiniCssExtractPlugin({
+            filename: "main.css"
+        }),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "src/index.html",
+            minify: false
+        })
+    ]
+}
